53b4ea745b1d1b7a07ba01f3b746311f1574e69b
NinakoLovu <leehtironi@gmail.com>
2018-08-29 16:03:53 -0300

    Adicionei mais resultados.

--- Testes/TesteMutex2/TesteMutex2.ino
+++ Testes/TesteMutex2/TesteMutex2.ino
@@ -1,47 +1,69 @@
 // Contador para ver quantos timers ele suporta;
+// Cria duas tarefas que irão ser deletadas após serem criadas.
+// Ficam fazendo isso sempre.
+
 #include <FreeRTOS_AVR.h>
 
 const uint8_t LED_PIN = 13;
-const int MAXT = 1;
-const int MAXV = 14;
+const uint8_t MAXT = 2;
+const uint8_t MAXV = 2;
 SemaphoreHandle_t xMutex[MAXV], xMMutex;
+unsigned long ulTimeStart = 0, ulTimeEnd = 0, ulTimeDif;
+unsigned long t1, t2;  
 //------------------------------------------------------------------------------
-
-static void vPrintTime(void* pvParameters){
-  int TimerId = (int)pvParameters;
-  unsigned long ulTimeStart = 0, ulTimeEnd = 0, ulTimeDif;
+  
+static void vGetTime(void* pvParameters){
+  uint8_t TimerId = (int)pvParameters;
+  uint8_t i;
   for(;;){
-    count[TimerId]++;
     xSemaphoreTake(xMMutex, portMAX_DELAY);
-    Serial.print(F("Meu id eh: "));
-    Serial.print(TimerId);
-    Serial.print(F("\n"));
     ulTimeStart = micros();
-    for(int i = 0; i < MAXV; i++){
+    for(i = 0; i < MAXV; i++){
       xSemaphoreTake(xMutex[i], portMAX_DELAY);
     }
-    ulTimeEnd = micros();
-    for(int i = 0; i < MAXV; i++){
+    for(i = 0; i < MAXV; i++){
       xSemaphoreGive(xMutex[i]); 
     }
-    ulTimeDif = ulTimeEnd - ulTimeStart;
-    Serial.print("Entrei na regiao critica no tempo: ");
-    Serial.print(ulTimeDif);
-    Serial.print("\n");
     xSemaphoreGive(xMMutex);
   }
 }
 
+static void vPrintTime(void* pvParameters){
+  uint8_t TimerId = (int)pvParameters;
+  uint8_t i;
+  for(;;){
+    xSemaphoreTake(xMMutex, portMAX_DELAY);
+    for(i = 0; i < MAXV; i++){
+      xSemaphoreTake(xMutex[i], portMAX_DELAY);
+    }
+    for(i = 0; i < MAXV; i++){
+      xSemaphoreGive(xMutex[i]); 
+    }
+    ulTimeEnd = micros();
+    //Serial.print(t1);
+    //Serial.print(F(" "));
+    //Serial.println(t2);
+    ulTimeDif = ulTimeEnd - ulTimeStart;
+    Serial.print("Entrei na regiao critica no tempo: ");
+    Serial.println(ulTimeDif);
+    xSemaphoreGive(xMMutex);
+    vTaskDelay(300);
+  }
+}
+
 //------------------------------------------------------------------------------
+
 void setup() {
+  uint8_t i;
   Serial.begin(9600);
   // wait for Leonardo
   while(!Serial) {}
   xMMutex = xSemaphoreCreateMutex();
-  for(int i = 0; i < MAXV; i++){
+  for(i = 0; i < MAXV; i++){
     xMutex[i] = xSemaphoreCreateMutex();
   }
-  for(int i = 0; i < MAXT; i++){
+  /*
+  for(i = 0; i < MAXT; i++){
     xTaskCreate( vPrintTime, // Nome da tarefa (funcao que a chama;
                               "TarefaTeste",      // Nome dado a tarefa;
                               200,                 // Quanto de pilha usar;
@@ -49,8 +71,24 @@ void setup() {
                               tskIDLE_PRIORITY+2,                      // Prioridade;
                               NULL
                               );
-  }
+  }*/
+
+  xTaskCreate(  vGetTime, // Nome da tarefa (funcao que a chama);
+                  "TarefaTeste",      // Nome dado a tarefa;
+                  200,                 // Quanto de pilha usar;
+                  (void*)0,
+                  tskIDLE_PRIORITY+2,                      // Prioridade;
+                  NULL );
+
+                  
+  xTaskCreate(  vPrintTime,
+                "TarefaQuePrinta",
+                200,
+                (void*)1,
+                tskIDLE_PRIORITY+3,
+                NULL );
   
+
   // start FreeRTOS
   vTaskStartScheduler();
 
@@ -58,6 +96,7 @@ void setup() {
   Serial.println(F("Die"));
   while(1);
 }
+
 //------------------------------------------------------------------------------
 // WARNING idle loop has a very small stack (configMINIMAL_STACK_SIZE)
 // loop must never block
