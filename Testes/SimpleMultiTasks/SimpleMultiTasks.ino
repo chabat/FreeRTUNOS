64e451f6077aa6617fe4e935bfa679ccc4370956
chabat <felipechabat@gmail.com>
2018-11-01 10:50:16 -0300

    Adicionado novos resultados para quantidade de tarefas por tamanho de pilha

--- Testes/SimpleMultiTasks/SimpleMultiTasks.ino
+++ Testes/SimpleMultiTasks/SimpleMultiTasks.ino
@@ -7,22 +7,22 @@ Authors: Felipe Chabatura Neto
 */
 
 #include <Arduino_FreeRTOS.h>
-#include <ArduinoTestSuite.h>
+//#include <ArduinoTestSuite.h>
 #define mainDELAY_LOOP_COUNT 112345 /* Usado como contador para o loop, para criar um delay */
-#define mainTASK_QUANTITY 1 /*Quantidade de tarefas a serem criadas*/
-#define mainSTACK_DEPTH 1089 /*Tamanho da pilha de cada tarefa em bytes*/
+#define mainTASK_QUANTITY 10 /*Quantidade de tarefas a serem criadas*/
+#define mainSTACK_DEPTH 100 /*Tamanho da pilha de cada tarefa em bytes*/
 
 void vTarefa(void *pvParametros);
 
 void setup() {
   int iId;
   
-  Serial.begin(9600); /* Define a taxa de bits por segundo para transmissão de dados */
+  //Serial.begin(9600); /* Define a taxa de bits por segundo para transmissão de dados */
 
   //Serial.println(ATS_GetFreeMemory());
 
   for(iId = 1; iId <= mainTASK_QUANTITY; iId++){
-    xTaskCreate( vTarefa, "Tarefa", mainSTACK_DEPTH, (void*) iId, 1, NULL);
+    xTaskCreate( vTarefa, NULL, mainSTACK_DEPTH, (void*) iId, 1, NULL);
   }
 
   //Serial.println(ATS_GetFreeMemory());  
@@ -36,13 +36,13 @@ void vTarefa(void *pvParametro){
   volatile unsigned long ul; /*Variável para o contador*/
   int iId = (int)pvParametro;
 
-  //for( ;; ){ /* Tarefas são criadas em loops infinitos */
-    Serial.print("Tarefa "); /* Imprime a string */
-    Serial.print(iId);
-    Serial.print(" Executando\n");
+  for( ;; ){ /* Tarefas são criadas em loops infinitos */
+    //Serial.print("Tarefa "); /* Imprime a string */
+    //Serial.print(iId);
+    //Serial.print(" Executando\n");
     
     for(ul = 0; ul < mainDELAY_LOOP_COUNT; ul++); /* Implementação bruta de um atraso */ 
-  //}
+  }
 }
 
 void loop() {}
